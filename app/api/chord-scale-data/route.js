674aee900fb02068010f9496462be50bb4ce5922
Nafeu Nasir <nafeu.nasir@gmail.com>
2025-02-03 21:53:20 -0500

    Add format and root option to chord-scale-data

--- app/api/chord-scale-data/route.js
+++ app/api/chord-scale-data/route.js
@@ -5,8 +5,40 @@ import {
   CHORD_COLLECTION,
 } from '../../components/ChordScaleExplorer/constants';
 
-export async function GET() {
-  return NextResponse.json([...CHORD_COLLECTION, ...SCALE_COLLECTION], {
-    status: 200,
-  });
+const DATA = [...CHORD_COLLECTION, ...SCALE_COLLECTION];
+
+const convertToCSV = (data) => {
+  const headers = 'root,name,notes,tags,type';
+  const rows = data
+    .map(
+      (item) =>
+        `${item.root},${item.name},"${item.notes.join(' ')}","${item.tags.join(' ')}",${item.type}`,
+    )
+    .join('\n');
+
+  return `${headers}\n${rows}`;
+};
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const format = searchParams.get('format');
+  const roots = searchParams.get('all');
+
+  let chordScaleData = DATA.filter(({ root }) => root === 'C');
+
+  if (roots === 'all') {
+    chordScaleData = DATA;
+  }
+
+  if (format === 'csv') {
+    const csvData = convertToCSV(chordScaleData);
+    return new NextResponse(csvData, {
+      status: 200,
+      headers: {
+        'Content-Type': 'text/csv',
+      },
+    });
+  }
+
+  return NextResponse.json(chordScaleData, { status: 200 });
 }
